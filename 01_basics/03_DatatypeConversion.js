6a9b67813ffc4e04a8852fca473166c521ec9b37
Korban Sheikh <52041478+mkorban2@users.noreply.github.com>
2024-08-09 16:48:05 +0000

    addOperation

--- 01_basics/03_DatatypeConversion.js
+++ 01_basics/03_DatatypeConversion.js
@@ -3,3 +3,23 @@ let number = 33
 let stringNumber = String(number)
 
 console.log(typeof stringNumber);
+
+
+
+//******operation*****
+
+//postIncrement
+
+let a = 4
+let b = a++
+
+console.log("a = " + a, "\nb = " + b);
+
+
+//preIncrement
+
+let x = 4
+let y = ++x
+
+console.log("x = " + x, "\ny = " + y);
+
