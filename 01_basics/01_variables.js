42772d32e912e9ee48da727b0e4821cf446a0800
Korban Sheikh <52041478+mkorban2@users.noreply.github.com>
2024-08-09 16:07:49 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountID = 12345
+let accountEmail = "korban@gmail.com"
+var accountPassword = 1234
+
+accountCity = "Dhaka"
+
+let accountState
+
+// accountID = 6
+
+accountEmail = "Mitu@gmail.com"
+accountPassword = 4321
+accountCity = "Jamalpur"
+
+
+console.log(accountID);
+
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all JS code new Version
+
+let nameOfclient = "Korban"
+let age = 30
+let office = null
+
+let home
+
+
+console.log(typeof null);
+
+
