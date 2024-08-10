826f2c65ab9d487cf35846d7586eb5afba761284
Korban Sheikh <52041478+mkorban2@users.noreply.github.com>
2024-08-10 14:06:48 +0000

    number and maths

---
+++ 01_basics/05_numandSum.js
@@ -0,0 +1,16 @@
+// const score = 500
+
+// const balance = new Number(100785200)
+// console.log(score);
+// console.log(balance.toLocaleString('en-IN'));
+
+
+
+//***************************Maths****************** */
+
+// console.log(Math.random())
+console.log(Math.floor(2.6))
+console.log(Math.ceil(2.6));
+console.log(Math.round(2.6));
+console.log(Math.round(2.4));
+
