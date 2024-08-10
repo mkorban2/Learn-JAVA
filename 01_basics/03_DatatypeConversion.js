cbfd6dc840f6744511ceb95ab62f60f73793c27f
Korban Sheikh <52041478+mkorban2@users.noreply.github.com>
2024-08-10 12:36:37 +0000

    Learn about String

--- 01_basics/03_DatatypeConversion.js
+++ 01_basics/03_DatatypeConversion.js
@@ -2,7 +2,7 @@ let number = 33
 
 let stringNumber = String(number)
 
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
 
 
 
@@ -13,7 +13,7 @@ console.log(typeof stringNumber);
 let a = 4
 let b = a++
 
-console.log("a = " + a, "\nb = " + b);
+//console.log("a = " + a, "\nb = " + b);
 
 
 //preIncrement
@@ -21,5 +21,25 @@ console.log("a = " + a, "\nb = " + b);
 let x = 4
 let y = ++x
 
-console.log("x = " + x, "\ny = " + y);
+//console.log("x = " + x, "\ny = " + y);
+
+
+//Data Types
+
+// Primitive (String, Number, Boolean, Null, Undefined, Symbol, BigInt)
+
+
+// const id = Symbol('123')
+
+// //console.log(id);
+
+// const score = Symbol (120)
+
+// console.log(score);
+
+//Non Primitive (Object, Array,Functions)
+
+
+
+
 
---
+++ 01_basics/04_Strings.js
@@ -0,0 +1,14 @@
+const name = "korban"
+const repoCount = 50
+
+
+console.log(`The name is ${name} and Repo Count is ${repoCount}`)
+
+const gameName = new String("  Korban  ")
+
+console.log(gameName.length);
+console.log(gameName.charAt(4))
+console.log(gameName.trim())
+console.log(gameName);
+
+
