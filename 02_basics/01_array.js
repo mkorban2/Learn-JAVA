2a55aa08f1a6f7b63dbc543abb9efa7dc941e5a9
Korban Sheikh <52041478+mkorban2@users.noreply.github.com>
2024-08-10 17:05:14 +0000

    learn array methods

---
+++ 02_basics/01_array.js
@@ -0,0 +1,18 @@
+const myArr = [2,6,8,3,5]
+
+// console.log(myArr)
+
+// myArr.push(5)
+// console.log(myArr)
+// myArr.pop()
+// console.log(myArr)
+
+
+let myN1 = myArr.slice(1,3)
+
+console.log(myN1);
+
+let myN2 = myArr.splice(1, 3)
+
+console.log(myN2);
+
