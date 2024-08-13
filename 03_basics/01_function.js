2a5be2de6485e21fc520d9fba464e01471bfd8dc
Korban Sheikh <52041478+mkorban2@users.noreply.github.com>
2024-08-13 14:23:45 +0000

    nested function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,12 @@
+function one(){
+    const fullName="korban"
+    function two(){
+        const newUser = "Ali"
+        console.log(fullName)
+    }
+    // console.log(newUser);
+    two()
+    
+}
+one();
+
